97c22e70927a251a5cdeea3df031867692842b54
NaveedAfraz <naveedafraz123@gmail.com>
2025-01-11 18:26:45 +0530

    added

--- frontend/src/store/admin-Slice/admin-slice.js
+++ frontend/src/store/admin-Slice/admin-slice.js
@@ -1,1 +1,25 @@
- 
+const { createSlice } = require("@reduxjs/toolkit");
+
+const initialState = {
+  productList: [],
+  loading: false,
+  error: null,
+};
+const adminProductSlice = createSlice({
+  name: "adminProduct",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getAdminProducts.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(getAdminProducts.fulfilled, (state, action) => {
+      (state.loading = false), (state.productList = action.payload.products);
+    });
+    builder.addCase(getAdminProducts.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    });
+  },
+});
