846cfcce327e46ab4849a441473c1702107332f9
NaveedAfraz <naveedafraz123@gmail.com>
2025-01-13 15:12:28 +0530

    added

--- frontend/src/components/ShoppingLayout/header.jsx
+++ frontend/src/components/ShoppingLayout/header.jsx
@@ -66,7 +66,7 @@ function MenuItems() {
 
 function HeaderRightContent() {
   const { user } = useSelector((state) => state.auth);
-  const { cartItems } = useSelector((state) => state.shopCart);
+//const { cartItems } = useSelector((state) => state.shopCart);
   const [openCartSheet, setOpenCartSheet] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
