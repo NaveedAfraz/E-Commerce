c3b8737e9c3b1ab6883c0436ca2958135ed48b82
NaveedAfraz <naveedafraz123@gmail.com>
2025-01-25 11:07:05 +0530

    Switch to bcryptjs for better compatibility

--- backend/helper/paypal.js
+++
@@ -1,7 +0,0 @@
-// const paypal = require("@paypal/checkout-server-sdk");
-
-// const environment = new paypal.core.SandboxEnvironment(
-//   "Afdk2Zb93EfHmvaydFH7Zo3s5roUH7r-Ng6lblqRRbK1ULnQFPMxLMflOAVJm4orl6G5lMJR_X20fpm8",
-//   "EJXeU9gwN8lBmwIQ8dqNJ78dBAgVmxI7ApM0LORl7a52zE5CeQquQEEDnPdgeX7pDUV4Z66JXT8_OxDk"
-// );
-// const client = new paypal.core.PayPalHttpClient(environment);
