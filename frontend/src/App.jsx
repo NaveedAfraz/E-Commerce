85e5e852ea8382736208698438002f102fe3c600
NaveedAfraz <naveedafraz123@gmail.com>
2025-06-06 15:15:38 +0530

    Fixed API URL and other changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,15 +14,16 @@ import Listings from "./pages/shopping/listings";
 import Account from "./pages/shopping/account";
 import AuthCheck from "./components/common/authCheck";
 import { useDispatch, useSelector } from "react-redux";
+import { Analytics } from "@vercel/analytics/react"
 import React, { useEffect } from "react";
 import { authCheck } from "./store/auth-Slice/auth-slice";
-import { Skeleton } from "@/components/ui/skeleton";
+import { Skeleton } from "@/components/ui/skeleon";
 import UnAuthorized from "./pages/authCheck/unAuthorized";
 import Paypalreturn from "./pages/shopping/paypal-return";
 import PayPalCancel from "./pages/shopping/paypal-cancel";
 import PaymentSuccess from "./pages/shopping/payment-sucess";
 import ShoppingOrders from "./components/ShoppingLayout/ShoppingOrders";
-import { Address } from "./components/ShoppingLayout/address";
+import { Address } from "./components/ShoppingLtayout/address";
 import Search from "./pages/shopping/search";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
@@ -49,71 +50,73 @@ export default function App() {
       </div>
     );
   }
-  
+
 
   return (
-    <> </>
-    <Router>
-      <Routes>
-        {/* Auth Routes */}
-        <Route
-          path="/"
-          element={<AuthCheck isAuthenticated={isAuth} user={user}></AuthCheck>}
-        />
-        <Route
-          path="/auth"
-          element={
-            <AuthCheck isAuth={isAuth} user={user}>
-              <Layout />
-            </AuthCheck>
-          }
-        >
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Route>
-
-        {/* Admin Routes */}
-        <Route
-          path="/admin"
-          element={
-            <AuthCheck isAuth={isAuth} user={user}>
-              <AdminLayout />
-            </AuthCheck>
-          }
-        >
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="product-list" element={<ProductList />} />
-          <Route path="orders" element={<Orders />} />
-          <Route path="features" element={<Features />} />
-        </Route>
-
-        {/* Shopping Routes */}
-        <Route
-          path="/shopping"
-          element={
-            <AuthCheck isAuth={isAuth} user={user}>
-              <ShoppingLayout />
-            </AuthCheck>
-          }
-        >
-          <Route path="home" element={<Home />} />
-          <Route path="checkout" element={<Checkout />} />
-          <Route path="search" element={<Search />} />
-          <Route path="listings" element={<Listings />} />
-          {/* Fixed Account routes */}
-          <Route path="account" element={<Account />}>
-            <Route path="orders" element={<ShoppingOrders />}></Route>
-            <Route path="address" element={<Address />}></Route>
+    <>
+      <Analytics />
+      <Router>
+        <Routes>
+          {/* Auth Routes */}
+          <Route
+            path="/"
+            element={<AuthCheck isAuthenticated={isAuth} user={user}></AuthCheck>}
+          />
+          <Route
+            path="/auth"
+            element={
+              <AuthCheck isAuth={isAuth} user={user}>
+                <Layout />
+              </AuthCheck>
+            }
+          >
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
           </Route>
-          <Route path="paypal-return" element={<Paypalreturn />} />
-          <Route path="paypal-cancel" element={<PayPalCancel />} />
-          <Route path="payment-success" element={<PaymentSuccess />}></Route>
-        </Route>
 
-        {/* Error handling */}
-        <Route path="/unauth-page" element={<UnAuthorized />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
-      </Routes>
-    </Router>
+          {/* Admin Routes */}
+          <Route
+            path="/admin"
+            element={
+              <AuthCheck isAuth={isAuth} user={user}>
+                <AdminLayout />
+              </AuthCheck>
+            }
+          >
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="product-list" element={<ProductList />} />
+            <Route path="orders" element={<Orders />} />
+            <Route path="features" element={<Features />} />
+          </Route>
+
+          {/* Shopping Routes */}
+          <Route
+            path="/shopping"
+            element={
+              <AuthCheck isAuth={isAuth} user={user}>
+                <ShoppingLayout />
+              </AuthCheck>
+            }
+          >
+            <Route path="home" element={<Home />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="search" element={<Search />} />
+            <Route path="listings" element={<Listings />} />
+            {/* Fixed Account routes */}
+            <Route path="account" element={<Account />}>
+              <Route path="orders" element={<ShoppingOrders />}></Route>
+              <Route path="address" element={<Address />}></Route>
+            </Route>
+            <Route path="paypal-return" element={<Paypalreturn />} />
+            <Route path="paypal-cancel" element={<PayPalCancel />} />
+            <Route path="payment-success" element={<PaymentSuccess />}></Route>
+          </Route>
+
+          {/* Error handling */}
+          <Route path="/unauth-page" element={<UnAuthorized />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
